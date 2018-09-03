6edaa52749ab4dc517749c6cc6b4ae666b3f037f
Sweisian <rswei888@gmail.com>
2018-09-02 20:31:45 -0500

    makind redis work

--- countWords.py
+++ countWords.py
@@ -1,14 +1,7 @@
 import requests
-from rq import Queue
-from worker import conn
-
 
 def count_words_at_url(url):
     resp = requests.get(url)
     word_count = len(resp.text.split())
     print(word_count)
     return word_count
-
-
-q = Queue(connection=conn)
-result = q.enqueue(count_words_at_url, 'http://heroku.com')
