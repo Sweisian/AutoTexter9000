5146e4d787946f0552430b73ce415cacdee717a3
Sweisian <rswei888@gmail.com>
2018-09-02 20:35:06 -0500

    more redis testing

--- countWords.py
+++ countWords.py
@@ -3,5 +3,6 @@ import requests
 def count_words_at_url(url):
     resp = requests.get(url)
     word_count = len(resp.text.split())
-    print(word_count)
+    print("I LIKE CHICKEN, GIVE ME CHICKEN")
+    print("WORDS I COUNTED: " + word_count)
     return word_count
