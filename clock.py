2ff8da0a938e1b737687a25846b0a453d27638c1
Sweisian <rswei888@gmail.com>
2018-09-02 22:16:04 -0500

    more redis testing

--- clock.py
+++ clock.py
@@ -6,7 +6,7 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', seconds=20)
 def timed_job():
-    #enque_job()
+    enque_job()
     print('This job is run every minute.')
 
 sched.start()
